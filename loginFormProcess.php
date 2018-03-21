378ce6a28f3ec37d564c8e5cf02211d978c1b819
swelycz <samwelyczkowsky@gmail.com>
2018-03-21 08:24:15 -0400

    connection problem diagnosis test with phpinfo() #2

--- loginFormProcess.php
+++ loginFormProcess.php
@@ -1,5 +1,4 @@
 <?php session_start();
-<<<<<<< HEAD
   echo phpinfo();
   /*$serverName = "waystonepm.database.windows.net";
   $connectionOptions = array(
@@ -9,17 +8,6 @@
   );
   //Establishes the connection
   $conn = sqlsrv_connect($serverName, $connectionOptions);
-=======
-$serverName = "waystonepm.database.windows.net";
-$connectionOptions = array(
-    "Database" => "tenant_accounts",
-    "Uid" => "waystoneadmin",
-    "PWD" => "waystoneMGMT!"
-);
-//Establishes the connection
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-
->>>>>>> 85d25fdb5b6ba46643fb0ee7e8d14db32e5d8973
 
   $email = $_POST["email"];
   $pass = $_POST["pass"];
@@ -27,9 +15,5 @@ $conn = sqlsrv_connect($serverName, $connectionOptions);
   if ($email == "index") {
     header('Location: index.php');
     die();
-<<<<<<< HEAD
   } */
-=======
-  }
->>>>>>> 85d25fdb5b6ba46643fb0ee7e8d14db32e5d8973
 ?>
