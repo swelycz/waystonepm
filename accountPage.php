fae2708c42834c030983e82440416a382e86cc61
swelycz <samwelyczkowsky@gmail.com>
2018-03-22 04:19:47 -0400

    test login

---
+++ accountPage.php
@@ -0,0 +1,5 @@
+<?php include("templates/header.php")?>
+<p>
+  You logged in with this email: <?= $_SESSION['email']?>
+</p>
+<?php include("templates/footer.php")?>
--- loginFormProcess.php
+++ loginFormProcess.php
@@ -8,9 +8,9 @@
   );
   //Establishes the connection
   $conn = sqlsrv_connect($serverName, $connectionOptions);
-  $testQuery = "select * from tenant_login";
+  /*$testQuery = "select * from tenant_login";
   $stmt = sqlsrv_query($conn, $testQuery);
-  if ($testResults === false) {
+  if ($stmt === false) {
     die(print_r(sqlsrv_errors(), true));
   }
 
@@ -19,5 +19,38 @@
   }
 
   sqlsrv_free_stmt($stmt);
+*/
+  function loginUser(&$conn) {
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $pass = filter_var($_POST['pass'], FILTER_SANITIZE_SPECIAL_CHARS);
+
+    if (empty($email) || empty($pass)) {
+      return ['all fields required', false];
+    }
+
+    $q = "select * from tenant_login where email = '$email'";
+    $result = sqlsrv_query($conn, $testQuery);
+
+    if (!$result) {
+      return [$conn->error, false];
+    } elseif ($result->num_rows !== 1) {
+      return ['problem logging in', false];
+    } else {
+      $email = $result->fetch_object();
+    }
+  }
+
+  list($msg, $success) = loginUser($conn);
+  $_SESSION['msg'] = $msg;
+  $_SESSION['email'] = $_POST['email'];
+
+  if ($success) {
+    $location = 'accountPage.php';
+  } else {
+    $location = 'index.php';
+  }
+
+  sqlsrv_free_stmt($result);
+  header("Location: $location");
 
 ?>
