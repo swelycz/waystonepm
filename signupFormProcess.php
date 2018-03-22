8e1b6579174f14f8086cebca872384ba47d1e429
swelycz <samwelyczkowsky@gmail.com>
2018-03-22 15:43:27 -0400

    connecting updated signup form to database

--- signupFormProcess.php
+++ signupFormProcess.php
@@ -13,12 +13,20 @@
     $fname = filter_var($_POST['fname'], FILTER_SANITIZE_SPECIAL_CHARS);
     $mi = filter_var($_POST['mIni'], FILTER_SANITIZE_SPECIAL_CHARS);
     $lname = filter_var($_POST['lname'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $address = filter_var($_POST['address'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $city = filter_var($_POST['city'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $state = filter_var($_POST['state'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $zip = filter_var($_POST['zip'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $phone = filter_var($_POST['phone'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $day = filter_var($_POST['days'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $month = filter_var($_POST['months'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $year = filter_var($_POST['years'], FILTER_SANITIZE_SPECIAL_CHARS);
     $email = filter_var($_POST['email'], FILTER_SANITIZE_SPECIAL_CHARS);
     $confEmail = filter_var($_POST['confEmail'], FILTER_SANITIZE_SPECIAL_CHARS);
     $pass = filter_var($_POST['password'], FILTER_SANITIZE_SPECIAL_CHARS);
     $confPass = filter_var($_POST['confPass'], FILTER_SANITIZE_SPECIAL_CHARS);
 
-    if (empty($fname) || empty($mi) || empty($lname) || empty($email) || empty($confEmail) || empty($pass) || empty($confPass)) {
+    if (empty($fname) || empty($mi) || empty($lname) || empty($address) || empty($city) || empty($state) || empty($zip) || empty($phone) || empty($day) || empty($month) || empty($year) || empty($email) || empty($confEmail) || empty($pass) || empty($confPass)) {
       return ['all fields required', false];
     } else if ($email !== $confEmail) {
       return ['emails do not match', false];
@@ -36,8 +44,9 @@
     if (!is_null($row)) {
       return ['email is already registered', false];
     }
+    $DOB = $year . "-" . $month . "-" . $day;
     sqlsrv_free_stmt($result);
-    $q = "insert TENANT (FIRSTNAME,MI,LASTNAME,EMAIL) values('$fname','$mi','$lname','$email')";
+    $q = "insert TENANT (FIRSTNAME,MI,LASTNAME,PERM_ADDRESS,PERM_STATE,PERM_ZIP,PHONE,EMAIL,DOB) values('$fname','$mi','$lname','$address','$state','$zip','$phone','$email','$DOB')";
     $result = sqlsrv_query($conn, $q);
     if ($result === false) {
       die(print_r(sqlsrv_errors(), true));
